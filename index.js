2293e6e51f922bd63a807c9a5c5e57da1b98ff2d
chen <tzafrirhn@gmail.com>
2022-07-12 13:30:29 +0300

    fix port issue

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var express = require("express");
-var app = express();
-
-var port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
 // save data for JSON
 let users = ['chtz','user1','yaniv','alex'],companies = ['Microsoft','eWave','Amazon','eBay','YouTube'];
