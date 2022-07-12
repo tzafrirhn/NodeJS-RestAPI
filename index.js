cf0cc529771f659ca94f2bb52539f97b62cd1f6a
chen <tzafrirhn@gmail.com>
2022-07-12 14:09:19 +0300

    test build and deploy pipeline

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 // save data for JSON
-let users = ['chtz','user1','yaniv','alex'],companies = ['Microsoft','eWave','Amazon','eBay','YouTube'];
+let users = ['Chen','Linus','Yaniv','Yossi'];
+let companies = ['Microsoft','eWave','Amazon','eBay','YouTube','Netflix'];
 
 app.get('/', (req, res) => {
     res.send(`Hello World, from Express server ver. 1.0 <br> listening on port ${port}!`);
