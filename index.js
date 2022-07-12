f10c804ab2317d10b9beb887a22c1d4222caff22
chen <tzafrirhn@gmail.com>
2022-07-12 10:12:23 +0300

    fix port for azure

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ var port = process.env.PORT || 3000;
 // Where we will keep users and companies
 let users = ['chtz','user1','yaniv','alex'],companies = ['Microsoft','eWave','Amazon','eBay','YouTube'];
 
-app.listen(port, () => {
- console.log("Server running on port 3000");
+app.get('/', (req, res) => {
+    res.send(`Hello World, from Express server ver. 1.0 <br> listening on port ${port}!`);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World, from Express server ver. 1.0');
+//Return JSON for users
+app.get('/api/users', (req, res) => {
+    res.json(users)
 });
+
+//Return JSON for companies
+app.get('/api/companies', (req, res) => {
+    res.json(companies)
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
